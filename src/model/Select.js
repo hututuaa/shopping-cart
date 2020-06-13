ffc8b8cb3f720000e71cf9e77e05283bcc49b995
HuMinjuan <2219405444@qq.com>
2020-06-13 12:25:12 +0800

    解决购物车数量显示问题

--- src/model/Select.js
+++ src/model/Select.js
@@ -9,40 +9,39 @@ export default {
         // sumData:[]
     },
     effects: {
-        *sort({ payload }, {put }) {
+        *sort({ payload }, { put }) {
             const _chooseValue = payload.value;
-            console.log("11_chooseValue",_chooseValue)
+            console.log("11_chooseValue", _chooseValue)
             yield put({
                 type: "handleSort",
                 payload: { _chooseValue }
             });
         },
         *selectSize({ payload }, { put }) {
-            const {_newResult,clickSize,sumProducts} = payload;
+            const { _newResult, clickSize, sumProducts } = payload;
             yield put({
                 type: "chooseSize",
-                payload: { _newResult,clickSize,sumProducts}
+                payload: { _newResult, clickSize, sumProducts }
             });
         }
 
 
     },
     reducers: {
-        chooseSize: (state, { payload: {  _newResult,clickSize} }) => {
+        chooseSize: (state, { payload: { _newResult, clickSize } }) => {
             return {
                 ...state,
                 sortData: _newResult,
-                clickSize:clickSize,
-                // sumData:sumProducts
+                clickSize: clickSize,
 
             };
         },
         handleSort: (state, { payload: { _chooseValue } }) => {
-            let {sortData} = state;
+            let { sortData } = state;
             return {
                 ...state,
-                chooseValue:_chooseValue,
-                selectResult: (_chooseValue === "D-S" ?sortData:(_chooseValue === "H-t-L" ? (sortData.sort((a, b) => { return b.price - a.price })) : (sortData.sort((a, b) => { return a.price - b.price })))),
+                chooseValue: _chooseValue,
+                selectResult: (_chooseValue === "D-S" ? sortData : (_chooseValue === "H-t-L" ? (sortData.sort((a, b) => { return b.price - a.price })) : (sortData.sort((a, b) => { return a.price - b.price })))),
 
             };
         },
